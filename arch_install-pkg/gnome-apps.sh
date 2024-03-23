cc6248a0b93b3746e8c1b14bf8d0e7d20dd06bbb
iamnormi <automatecode@gmail.com>
2024-03-24 03:26:28 +0530

    Install scripts added

--- arch_install-pkg/gnome-apps.sh
+++ arch_install-pkg/gnome-apps.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 #########################install gnome desktop needed app: nautilus, gnome-disks
-sudo pacman -S nautilus gnome-disk-utility  gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb sshfs
-
+sudo pacman -S nautilus gnome-disk-utility  gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb sshfs filemanager-actions easytag brasero evince file-roller python-nautilus libpeas squashfs-tools lrzip p7zip unace unrar aspell hunspell libvoikko libvoikko nuspell 
