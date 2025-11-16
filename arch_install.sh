3bdc6e63cdc6fba0c28d6bb5e63f088f3fcbbbe0
Vinod Kumar <71950371+iamnormi@users.noreply.github.com>
2025-11-17 01:10:39 +0530

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -2,7 +2,7 @@
 # == MY ARCH SETUP INSTALLER == #
 #part1
 printf '\033c'
-echo "Welcome to iamvk1437k's arch installer script"
+echo "Welcome to iamnormi's arch installer script"
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
 pacman --noconfirm -Sy archlinux-keyring
 loadkeys us
@@ -93,7 +93,7 @@ exit
 printf '\033c'
 cd $HOME
 #dotfiles
-git clone https://github.com/iamvk1437k/dotfiles ~/.local/src/dotfiles
+git clone https://github.com/iamnormi/dotfiles ~/.local/src/dotfiles
 rm -vrf ~/.config ; cp -vrf ~/.local/src/dotfiles/.config/ ~/
 cp -vrf ~/.local/src/dotfiles/.local/bin/ ~/.local/
 sudo cp -vrf ~/.local/src/dotfiles/etc/X11/xorg.conf.d/20-intel.conf /etc/X11/xorg.conf.d/20-intel.conf
@@ -104,15 +104,15 @@ sudo mkdir -pv /etc/NetworkManager/conf.d/
 sudo cp -vrf ~/.local/src/dotfiles/etc/NetworkManager/conf.d/any-user.conf /etc/NetworkManager/conf.d/any-user.conf 
 
 # dwm: Window Manager
-git clone https://github.com/iamvk1437k/dwm.git ~/.local/src/dwm
+git clone https://github.com/iamnormi/dwm.git ~/.local/src/dwm
 sudo make -C ~/.local/src/dwm install
 
 # st: Terminal
-git clone https://github.com/iamvk1437k/st.git ~/.local/src/st
+git clone https://github.com/iamnormi/st.git ~/.local/src/st
 sudo make -C ~/.local/src/st install
 
 # dmenu: Program Menu
-git clone https://github.com/iamvk1437k/dmenu.git ~/.local/src/dmenu
+git clone https://github.com/iamnormi/dmenu.git ~/.local/src/dmenu
 sudo make -C ~/.local/src/dmenu install
 
 # pikaur: AUR helper
